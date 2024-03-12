84cce5b5efd2c9b0533d9404d35942a0bdbcdade
efizlende <145128350+efizlende@users.noreply.github.com>
2024-03-12 13:22:46 +0200

    Add files via upload
    CRUD com Django

---
+++ CRUDJ/CRUDJ/urls.py
@@ -0,0 +1,13 @@
+
+from django.contrib import admin
+from django.urls import path
+from Crudapp import views  
+ 
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),  
+    path('addnew',views.addnew, name='addnew'),  
+    path('edit/<int:id>', views.edit, name='edit'),  
+    path('update/<int:id>', views.update, name='update'),  
+    path('delete/<int:id>', views.destroy, name='destroy'),  
+]
